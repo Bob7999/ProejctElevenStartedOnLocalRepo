63c9143b64483a9857b2de58e6273c7e746f7f98
Robert <bob.hindla@gmail.com>
2018-07-20 08:29:50 -0400

    Create file

---
+++ 2018-07-20-food.py
@@ -0,0 +1,40 @@
+# json again
+
+food = {
+    "chicken":{
+        "cornish_hen":{
+            "weight":5,
+            "size":12,
+        },
+        "rhode_island_red":{
+            "weight":6,
+            "size":12
+        },
+        "plain_old":{
+            "weight":13,
+            "size":12
+        }
+    },
+    "pork":{
+        "berkshire":{
+            "size":"four_feet",
+            "weight":"six_hundred",
+        },
+        "jersey":{
+            "size":"five_feet",
+            "weight":"twelve_hundred",
+        }
+    },
+    "Fish":{
+        "salmon":{
+            "size":"three_feet",
+            "weight":"thirty-five"
+        },
+        "cod":{
+            "size":"four_feet",
+            "weight":"forty-five"
+        }
+    }
+
+}
+
