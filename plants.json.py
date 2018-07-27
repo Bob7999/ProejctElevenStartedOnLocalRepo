dc478c0e5864c71c3b5cd249886a339d2390d780
Robert <bob.hindla@gmail.com>
2018-07-27 07:57:01 -0400

    Add plants json

---
+++ plants.json.py
@@ -0,0 +1,28 @@
+# this is a 
+
+plants = {
+    "version":"2012-10.02",
+    "trees":{
+        "deciduous":{
+            "maples":{
+                "types":[
+                    "red",
+                    "silver"
+                ],
+            "firs":{
+                "types":[
+                    "douglas",
+                    "blue"
+                ]
+            }
+        
+            }
+        },
+        "evergreen":{
+            "pines":[
+                "scotch", "black"
+            ]
+            
+        }
+    }
+}
