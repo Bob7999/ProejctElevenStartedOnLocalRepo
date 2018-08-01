814c110f564488ddc908b26e7e8317e30282bbb6
Robert <bob.hindla@gmail.com>
2018-08-01 08:02:54 -0400

    First Cut at Computers JSON

---
+++ computers1.py
@@ -0,0 +1,28 @@
+computers1 = {
+
+    "laptop":{
+        "dell":{
+            "dimension":{
+                "5100":{
+                    "diskless":{
+                        "components":[
+                            "screen:15inches",
+                            "dvd:hitachi"
+                        ],
+                    "entertainment":{
+                        "components":[
+                            "screen:144inches",
+                            "dvd:from_outerspace"
+                        ]    
+                    }
+                    }
+
+                }
+            }
+        }
+
+
+
+    }
+}
+
