3a5dfe423a4df7e72913ee2d38445ca32019077c
Robert <bob.hindla@gmail.com>
2018-08-03 11:40:34 -0400

    Fix to outerspace dictionary

---
+++ computers3.py
@@ -0,0 +1,38 @@
+#components is a list.  I can enter another dictionary-type
+#structure right after the closing brackets of components, under
+#the heading diskless.
+#Or, I can add a dictionary as the next element of the list
+
+
+computers3= {
+    "laptop":{
+        "dell":{
+            "dimension":{
+                "5100":{
+                    "diskless":{
+                        "components":[
+                            "screen:15inches",
+                            {"dvd":
+                                 {
+                                "format":"R-W",
+                                "format2":"R+W",
+                                 },
+                            },
+                            "goop_where_keyboard_was",       
+                           ],
+                    },
+                    "entertainment":{
+                        "components":[
+                            "screen:144inches",
+                            "dvd:from_outerspace"
+                        ]    
+                    }
+                    }
+
+                }
+            }
+        }
+
+    }
+
+
