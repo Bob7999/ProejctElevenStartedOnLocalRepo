00044c2bc297f4416b5bfdd0d4b95df8838d7dd2
Robert <bob.hindla@gmail.com>
2018-08-03 11:53:59 -0400

    Correct Inverted reversed square and curly braces

---
+++ computers2.py
@@ -0,0 +1,38 @@
+#components is a list.  I can enter another dictionary-type
+#structure right after the closing brackets of components, under
+#the heading diskless.
+#Or, I can add a dictionary as the next element of the list
+
+
+computers2 = {
+    "laptop":{
+        "dell":{
+            "dimension":{
+                "5100":{
+                    "diskless":{
+                        "components":[
+                            "screen:15inches",
+                            "dvd:hitachi",
+                            {"accessories":{
+                                "helmet":"orange",
+                                "gloves":"white",
+                                }
+                            },
+                        ]
+                            
+                        },
+                    "entertainment":{
+                        "components":[
+                            "screen:144inches",
+                            "dvd:from_outerspace"
+                        ]    
+                    }
+                    }
+
+                }
+            }
+        }
+ }
+
+
+
