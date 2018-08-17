20d6be1f492f179dbdd5b8f079449791b0a05798
Robert <bob.hindla@gmail.com>
2018-08-17 10:39:16 -0400

    Add fish file

---
+++ fish.py
@@ -0,0 +1,23 @@
+Fish = {
+        "Cartilogenous":{
+            "Rays":1,
+            "Sharks":{
+                "mid-ocean":{
+                    "blue-tip":1,
+                    "tiger":2,
+                },
+                "bottom-dwelling":{
+                    "nurse-shark":3,
+                    "seven-gill":{
+                        "ancient":4,
+                        "modern":5
+                    }
+                }
+                }
+            },
+            "Skates":{
+                "brown":6,
+                "black":7,
+        }
+    }
+
