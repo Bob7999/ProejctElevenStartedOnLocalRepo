3ca0c0e71208da409225b018157535584eb6734f
Robert <bob.hindla@gmail.com>
2018-08-23 08:27:13 -0400

    Overwrite-the-wrong-file-again

---
+++ 2018-08-23-sharks.py
@@ -0,0 +1,19 @@
+#sharks, i feel am swimming
+# with sharks.
+
+sharks = {
+
+        "Version":"2010-09-02",
+        "Ancient_sharks":{
+            "hairy-chested":{
+                "haired":"red-haired",
+                "bald":{
+                    "transmission":"eight-speed",
+                    "seats":{
+                        "reclining":"turquoise",
+                        "stationery":"red"
+                    }
+                }
+            }
+        }
+}
