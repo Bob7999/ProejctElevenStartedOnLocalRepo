5dcdc4fbeb947ac0fd547fc277dcdd7b78f26af0
Robert <bob.hindla@gmail.com>
2018-09-13 09:26:51 -0400

    straighten_out_the_use_of-lists_as_dictionary_items

--- 2018-09-12-colleges-json.py
+++ 2018-09-12-colleges-json.py
@@ -22,23 +22,27 @@ colleges = {
             "female":{
                 "left-handed":3500,
                 "right-handed":1500,
-                "hated_because_of_nose_shape":[
-                         "julia",
+                "those_who_are_hated":{
+                "hated_because_of_nose_shape":[    #key 1
+                         "julia",                  #item1
                          "rose",
                          "amanda",
                     ],
-                "hated_because_of_favorite_flavor":[
-                        "amy",
+                "hated_because_of_favorite_flavor":[      #key2
+                        "amy",                            #item2
                         "brandy",
                         "susan",
                     ],
+                }
             },
+        },
         "departments":{
             "whale_song":150,
             "chinese_checkers":1000,
             "particle_physics":400,
             },
         },
+    
     "Upenn":{
         "student_count":{
             "male":3000,
