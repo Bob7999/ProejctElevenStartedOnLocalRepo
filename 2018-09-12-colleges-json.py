3ef166d0d1df98deeba0bf72fbce758dfda04385
Robert <bob.hindla@gmail.com>
2018-09-13 08:39:34 -0400

    Colleges json

---
+++ 2018-09-12-colleges-json.py
@@ -0,0 +1,30 @@
+# colleges
+
+
+colleges = {
+
+    "yale":{
+        "student_count":15000,
+        "ratio":{
+            "male":10000,
+            "female":5000,
+        },
+        "departments":{
+            "English":300,
+            "Computer_Science":2000,
+            "Drama":5
+        }
+    },
+    "brown":{
+        "student_count":10000,
+        "ratio":{
+            "male":5000,
+            "female":5000,
+        },
+        "departments":{
+            "whale_song":150,
+            "chinese_checkers":1000,
+            "particle_physics":400,
+            },
+        }
+}
