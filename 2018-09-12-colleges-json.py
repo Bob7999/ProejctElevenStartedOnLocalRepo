2e8fa12f601a3ff3f1f15778da60edcdfd8a16b5
Robert <bob.hindla@gmail.com>
2018-09-13 08:46:29 -0400

    count left and right hande women

--- 2018-09-12-colleges-json.py
+++ 2018-09-12-colleges-json.py
@@ -19,12 +19,34 @@ colleges = {
         "student_count":10000,
         "ratio":{
             "male":5000,
-            "female":5000,
+            "female":{
+                "left-handed":3500,
+                "right-handed":1500,
+            }
         },
         "departments":{
             "whale_song":150,
             "chinese_checkers":1000,
             "particle_physics":400,
             },
+        },
+    "Upenn":{
+        "student_count":{
+            "male":3000,
+            "female":3000,
+        },
+        "favorite_food":{
+            "cheese_steak":500,
+            "jam_butties":1000,
+            "luxurious_cuisine":4500,
+            },
+        "favorite_cars":[
+            "ford:fiesta",
+            "chrysler:300",
+            "vw:passat",
+            "buick:lesabre",
+        ]
         }
 }
+    
+
