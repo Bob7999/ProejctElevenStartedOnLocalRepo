2e493751a6bde03f8f6242c949911239b05ced86
Robert <bob.hindla@gmail.com>
2018-09-19 08:25:16 -0400

    begin grocery list

---
+++ 208-09-19-grocery-list.py
@@ -0,0 +1,20 @@
+#grocery_list
+
+groceries = {
+
+            "vegetables":[
+                "asparagus",
+                "broccolli",
+                "kale",
+            ],
+            "meat":{
+                "four-legged":{
+                    "beef":"hereford",
+                    "sheep":"merino",
+                },
+                "no-legged":{
+                    "whale:blue",
+                }
+            
+}
+}
