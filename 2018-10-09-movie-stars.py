de491a99b2b5f87df7228e289e3aad34924c997c
Robert <bob.hindla@gmail.com>
2018-10-09 08:22:10 -0400

    add movie stars

---
+++ 2018-10-09-movie-stars.py
@@ -0,0 +1,30 @@
+Movie_Stars = {
+
+            "Era":{
+
+                    "1940s":{
+                        1:"Humphrey Bogart",
+                        2:"Lauren Bacall"
+
+                    },
+
+                    "1950s":{
+                        1:"Willard Peck",
+                        2:"Marilyn Monroe"
+                    },
+                    
+                    "1960s":{
+                        1:"Ali MacGraw",
+                        2:"Steve McQueen"
+                    },
+
+                    "1970s":{
+                        1:"Susan St. James",
+                        2:"Rock Hudson"
+                    }
+
+
+                    }
+
+}
+
