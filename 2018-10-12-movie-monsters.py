2cd1ccdb84a0c4830a9d1b282c6796b76301e2f9
Robert <bob.hindla@gmail.com>
2018-10-12 08:51:50 -0400

    Create movie monsteres

---
+++ 2018-10-12-movie-monsters.py
@@ -0,0 +1,18 @@
+movie_monsters = {
+
+    "man-shaped":[
+        "wolfman",
+        "dracula",
+        "monster_from_the_black_lagoon"
+    ],
+
+    "shapeless, but material":[
+        "the blob",
+        "the green slime"
+    ],
+
+    "female-movie-monsters":[
+        "The 50-Foot Woman"
+    ]
+
+}
