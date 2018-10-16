22a00c6c1f261901d0d7ba799ea60d927f01effc
Robert <bob.hindla@gmail.com>
2018-10-16 08:08:18 -0400

    Prove a dictionary cn be a list member

--- 2018-10-12-movie-monsters.py
+++ 2018-10-12-movie-monsters.py
@@ -1,18 +1,1 @@
-movie_monsters = {
-
-    "man-shaped":[
-        "wolfman",
-        "dracula",
-        "monster_from_the_black_lagoon"
-    ],
-
-    "shapeless, but material":[
-        "the blob",
-        "the green slime"
-    ],
-
-    "female-movie-monsters":[
-        "The 50-Foot Woman"
-    ]
-
-}
+"2018-10-12-movie-monsters.json"
