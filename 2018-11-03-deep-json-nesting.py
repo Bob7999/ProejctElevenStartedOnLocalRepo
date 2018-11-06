3105253c43412cba844a5f731fedebdc1513ed1d
Robert <bob.hindla@gmail.com>
2018-11-06 07:54:17 -0500

    Add new vehicles roster

---
+++ 2018-11-03-deep-json-nesting.py
@@ -0,0 +1,55 @@
+#Deep nesting
+
+the_deeps = {
+    "vehicles":{
+        "horse-drawn":[
+            "ice_wagon",
+            "vegetable_wagon",
+            "paddy_wagon",
+            "beer_wagon"
+        ],
+        "gasoline_powered":{
+            "car":{
+                "buick":{
+                    "electra":{
+                        "trim_level":"platinum-leaf_door_handles"
+                    }
+                }
+            }
+        }
+        
+    },
+    "lighter_than_air":[
+        {"type":"balloon",
+        "shape":"spherical",
+        "manufacturer":"Montgolfier Bros. Inc"},
+
+        {"type":"anti-grav_gondola",
+        "cost":"one sky-scraper-per-second"}
+
+    ],
+
+    "heavier_than_air":[
+        {"airplane":{
+            "lockheed":{
+                "fighter":{
+                    "F-104G":"Starfighter"
+                }
+            }
+        }
+        },
+
+        {"Liquid_Fuel_Rocket":{
+            "NASA":{
+                "APPOLO":{
+                    "Saturn V":"Liquid Oxygen"
+                }
+            }
+        }
+        }
+              
+    ]
+
+}
+
+
