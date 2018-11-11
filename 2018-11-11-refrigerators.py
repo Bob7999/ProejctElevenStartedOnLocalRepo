9febbe15fc2e8c867178cc79d034b5697b73b823
Robert <bob.hindla@gmail.com>
2018-11-11 16:33:15 -0500

    Add list of refrigerators and their colors

---
+++ 2018-11-11-refrigerators.py
@@ -0,0 +1,30 @@
+#refigerators
+refrigerators = {
+...
+...     "amana":[
+...         "avocado",
+...         "teal",
+...         "moonlight"
+...     ],
+...     "subzero":[
+...         "eggplant",
+...         "kale",
+...         "arugula"
+...     ],
+...     "Ichi-Ban":[
+...         "ducks_feet yellow",
+...         "steer_blood_brick_red",
+...         "goat_liver_green"
+...     ],
+...     "General_Electric":{
+...         "Freezer":{
+...             "Ice_Tray":{
+...                 "size":[
+...                     "small",
+...                     "medium",
+...                     "large"
+...                 ]
+...             }
+...         }
+...     }
+... }
