537f3536c7d826399037ac583508f152d39e3aa9
Robert <bob.hindla@gmail.com>
2018-11-16 08:38:18 -0500

    document kinds of math

---
+++ 2018-11-16-json-for-math.py
@@ -0,0 +1,40 @@
+#json structure for math topics
+
+math = {
+    "geometry":{
+        "aristotle":{
+            "aristotles_grandchildren":{
+                "hairy":[
+                    "square",
+                    "circle",
+                    "trapezoid"
+                ]
+            }
+        }
+    },
+    "calculus":{
+        "Leibnez":{
+            "Leibnez_Grandchildren":{
+                "matilda":{
+                    "matilda's_dolls":[
+                        "sarah",
+                        "becca"
+                    ]
+                }
+            }
+            }
+        
+
+        },
+
+
+    "trigonmetry":{
+        "type_of_science":{
+            "boring":[
+                "dull",
+                "snore-engendering"
+            ]
+        }
+    }
+
+}
