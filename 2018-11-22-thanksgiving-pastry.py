3d7983e5da078658d8a4c1daf2432061eaf9a335
Robert <bob.hindla@gmail.com>
2018-11-22 09:05:03 -0500

    Add 'main ingredient' section

---
+++ 2018-11-22-thanksgiving-pastry.py
@@ -0,0 +1,17 @@
+#pastries
+
+pastries  = {
+    "nationality":{
+        "french":[
+            "puff",
+            "peach",
+            "chocolate"
+        ],
+        "english":[
+            "gloppy",
+            "shiny",
+            "arrogant"
+        ]
+    }
+}
+    
---
+++ 2018-11-22-thanksgiving-pastry2.py
@@ -0,0 +1,30 @@
+#pastries
+
+pastries  = {
+    "nationality":{
+        "french":[
+            "puff",
+            "peach",
+            "chocolate"
+        ],
+        "english":[
+            "gloppy",
+            "shiny",
+            "arrogant"
+        ]
+    },
+    "main_ingredient":{
+         "french_pastry":[
+             "creme",
+             "eggs",
+             "broken_crocks, just for the Anglaise"
+         ],
+         "english_pastry":[
+             "upper_class_snobbery",
+             "butter_glaze"
+         ]
+
+
+    }
+}
+    
