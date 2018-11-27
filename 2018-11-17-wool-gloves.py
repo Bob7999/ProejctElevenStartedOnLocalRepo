47987e2f789a34b85ec009047d03aa90fc0b82d3
Robert <bob.hindla@gmail.com>
2018-11-27 08:36:44 -0500

    Add info on gloves

---
+++ 2018-11-17-wool-gloves.py
@@ -0,0 +1,26 @@
+#gloves:  nationality, style, contents, bio/non-biodegradeble
+
+gloves = {
+    "British":{
+        "style":[
+            "long",
+            "mourning",
+            "Sheffield"],
+        "contents":{
+            "wool":{
+                "coarse":"white",
+                "fine":"black"
+            }
+        }
+        
+
+
+    },
+    "French":{
+
+    },
+    "Dutch":{
+    }
+
+
+}
