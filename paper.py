714131d38c6944e5e9b1313165f1bce04d617557
Robert <bob.hindla@gmail.com>
2018-12-13 07:52:39 -0500

    added kinds of paper

---
+++ paper.py
@@ -0,0 +1,25 @@
+#Paper
+
+paper={
+    "copier_paper":{
+        "color":{
+            "red":{
+                "size":[
+                    "a4","legal","letter"
+                ]
+            }
+        }
+    },
+    "cardboard":{
+        "brown":{
+            "corrugated":{
+                "heavy_duty":{
+                    "has_attended_peace_camp":"yes"
+            }
+            }
+        }
+    }
+
+
+
+}
