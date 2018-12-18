8f003b1787678ca0273fab293c9b8f024ea54e7f
Robert <bob.hindla@gmail.com>
2018-12-18 07:21:39 -0500

    Add Game fish

---
+++ 2018-12-18-Game-Fish.py
@@ -0,0 +1,18 @@
+#game fish
+game_fish = {
+
+    "tarpon":{
+        "fins":3,
+        "mouth":1,
+        "colors":[
+            "red", "mucilege brown", "startling_sapphire", "avocado"
+        ]
+    },
+    "sword_fish":{
+        "obsolete_colors":[
+            "MaryMount_Blue","Sunrise_Orange_Haze","Monet_Lavender"
+        ],
+        "Sharpness":"Very_Sharp",
+        "Coat_to_Wear_in_Cold_Water":"DownCoat"
+    }
+}
