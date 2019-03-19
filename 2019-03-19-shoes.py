cec416bc8bd2698852b0375e237ba0e32414f560
Robert <bob.hindla@gmail.com>
2019-03-19 10:29:19 -0400

    Add comments on shoes

---
+++ 2019-03-19-shoes.py
@@ -0,0 +1,38 @@
+shoes = {
+        "Version":"2012-07-02",
+        
+        "comment":"types_of_shoes",
+        "shoes":{
+            "sandals":{
+                "material":{
+                    "vegetable":"palm",
+                    "synthetic":{
+                        "rubber":"neoprene"
+                    }
+                }
+            }                      
+        },
+        "boots":{
+            "type":{
+                "army":{
+                    "nationality":{
+                        "uruguayan"
+                    }
+                }
+            }
+        },
+        "slippers":{
+            "celebrity":[
+                "Liberace",
+                "Elton John",
+                "Jamairoquai"
+            ],
+            "hospital":[
+                "LI Jewish",
+                "Good Sam"
+            ]
+
+        }
+}
+
+
