fcccaddf2ffeb657cd1ecd9172c14d36ed0d0c75
Robert <bob.hindla@gmail.com>
2019-03-21 09:25:27 -0400

    Fix all the errors with an extra pair of braces

---
+++ 2019-01-09-ver2-shoes-enhanced-.py
@@ -0,0 +1,19 @@
+#Categories of shoe
+
+shoe = {
+    "Version":"10.2.2",
+    "Statement":[
+        
+        "boots":{
+            "tough":1
+            }
+        },
+        "dancing_shoes":{
+                "comfortable":"ambassador_heel"
+        },
+        "construction":{
+            "tough":"carhartt"
+        },
+    ]
+        }
+
