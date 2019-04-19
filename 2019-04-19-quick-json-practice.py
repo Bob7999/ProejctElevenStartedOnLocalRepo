79d199cf267eb30e7ba4bd041185e6f89d1a9a5a
Robert <bob.hindla@gmail.com>
2019-04-19 09:41:35 -0400

    Just practice

---
+++ 2019-04-19-quick-json-practice.py
@@ -0,0 +1,19 @@
+# quick JSON
+
+frogs = {
+
+    "large":"bullfrog",
+    "tiny":"peeperfrogs",
+    "minority_frogs":["migos","crems","bojis","habalistas"],
+    "frog_shoes":{
+        "crawling_shoes":1,
+        "climbing_shoes":{
+            "rubber":"sinalast",
+            "gum":"duroma",
+            "rubber_cement":{
+                3:["why", "are", "you", "a", "helpless", "retard"],
+                4:["without","electricity","we", "are", "helpless"]
+            }
+        }
+    }
+}
