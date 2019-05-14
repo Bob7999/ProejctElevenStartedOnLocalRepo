ce9a0f4589953ceda43be8a017c979d6a1870efa
Robert <bob.hindla@gmail.com>
2019-05-14 11:01:40 -0400

    putsch

---
+++ 2019-05-14-putsch.py
@@ -0,0 +1,21 @@
+putsch = {
+    "malevolent":[1,2,3],
+    "mammoth": "extinct",
+    "cats":{
+        "large":{
+            "angry":{
+                "tiger":"furious"
+            }
+        }
+    }
+}
+
+Bunch = {
+		{
+            "SnapToRestore" : {
+			   "Type" : "String",
+			    "Default" : "3",
+			    "Description" : "snap id to restore",
+		    }
+        }
+}
